7c6f381a19ddf692e2506a2394bf325622e418ae
peterjaap <peterjaap@elgentos.nl>
2026-03-29 10:54:34 +0200

    Fix Homebrew formula audit errors for multi-platform installs
    Replace `if Hardware::CPU.arm?` with `on_arm`/`on_intel` DSL blocks
    and move `def install` to class level to fix "Do not define methods
    in blocks" brew audit errors.

--- dist/homebrew/magebox.rb
+++ dist/homebrew/magebox.rb
@@ -8,39 +8,34 @@ class Magebox < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-darwin-arm64"
       sha256 "PLACEHOLDER_DARWIN_ARM64"
-
-      def install
-        bin.install "magebox-darwin-arm64" => "magebox"
-      end
-    else
+    end
+    on_intel do
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-darwin-amd64"
       sha256 "PLACEHOLDER_DARWIN_AMD64"
-
-      def install
-        bin.install "magebox-darwin-amd64" => "magebox"
-      end
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-linux-arm64"
       sha256 "PLACEHOLDER_LINUX_ARM64"
-
-      def install
-        bin.install "magebox-linux-arm64" => "magebox"
-      end
-    else
+    end
+    on_intel do
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-linux-amd64"
       sha256 "PLACEHOLDER_LINUX_AMD64"
-
-      def install
-        bin.install "magebox-linux-amd64" => "magebox"
-      end
     end
+  end
+
+  def install
+    suffix = if OS.mac?
+               Hardware::CPU.arm? ? "darwin-arm64" : "darwin-amd64"
+             else
+               Hardware::CPU.arm? ? "linux-arm64" : "linux-amd64"
+             end
+    bin.install "magebox-#{suffix}" => "magebox"
   end
 
   test do
